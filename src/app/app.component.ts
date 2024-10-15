26cf488021a011db41d92aaf28f5ede8ab777650
Gokul <vannangokul@gmail.com>
2024-10-15 21:57:41 +0530

    initial app component changed from stand alone component to modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
 import { User } from './user/user.model';
+
+
+// import { HeaderComponent } from "./header/header.component";
+// import { UserComponent } from "./user/user.component";
+// import { TasksComponent } from "./tasks/tasks.component";
 
 
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
