d9a193694471af94817897bd29d34963edc9b88a
Gokul <vannangokul@gmail.com>
2024-10-16 00:22:10 +0530

    removed old section and added section 3 starting project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Essentials
+# EssentialsPractice
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.0-next.2.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,8 @@
 import { Component } from '@angular/core';
-import { DUMMY_USERS } from './dummy-users';
-import { User } from './user/user.model';
-
-
-// import { HeaderComponent } from "./header/header.component";
-// import { UserComponent } from "./user/user.component";
-// import { TasksComponent } from "./tasks/tasks.component";
-
-
 
 @Component({
   selector: 'app-root',
-  standalone: false,
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  users = DUMMY_USERS;
-  selectedUser: User = {avatar:'', id:'', name:''};
-
-  isSelected(user: User){ 
-    return user.id === this.selectedUser?.id;
-  }
-
-  onSelect(id: string){
-    this.selectedUser = this.users.find(elem => elem.id == id )!;
-  }
-  // onTaskCompletion(id: string){
-  //   this.users =  this?.users?.filter(elem => elem.id != id)!;
-  // }
-}
+export class AppComponent {}
