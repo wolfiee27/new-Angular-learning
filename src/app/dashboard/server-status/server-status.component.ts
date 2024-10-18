8a58ce62fd8699704e94ce1f82e930f11646c1d6
Gokul <vannangokul@gmail.com>
2024-10-19 01:04:14 +0530

    ng OnInIt lifecyle hook

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
   currentStatus = 'online';
+
+  constructor() {}
+  ngOnInit(): void {
+    setInterval(() => {
+      const rand = Math.random();
+      if (rand < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rand < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 4000);
+  }
 }
