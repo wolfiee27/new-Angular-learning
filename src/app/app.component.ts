8cf58161950fc1e110339b638c0d59a3aceae76e
Gokul <vannangokul@gmail.com>
2024-10-18 17:00:36 +0530

    fixed dashboard renaming  / created common daskboard-item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
-import { ServerStatusComponent } from "./daskboard/server-status/server-status.component";
-import { TrafficComponent } from "./daskboard/traffic/traffic.component";
-import { TicketsComponent } from "./daskboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashboard-item/dashboard-item.component";
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
+
 })
 export class AppComponent {
 
---
+++ src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-tickets.component.html',
+  styleUrl: './new-tickets.component.css'
+})
+export class NewTicketsComponent {
+
+}
