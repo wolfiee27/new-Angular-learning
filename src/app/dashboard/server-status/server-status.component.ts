50eee01ef60c8b2ea4b71f00e719cdb65453975c
Gokul <vannangokul@gmail.com>
2024-10-19 15:07:40 +0530

    subscribe to signal changes

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, effect, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,19 +8,23 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent implements OnInit, OnDestroy {
-  currentStatus = 'online';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
   private timeout?: ReturnType<typeof setInterval>;
-  constructor() {}
+  constructor() {
+    effect(() => {
+      console.log('currentServerStaus- ' + this.currentStatus());
+    });
+  }
 
   ngOnInit(): void {
     this.timeout = setInterval(() => {
       const rand = Math.random();
       if (rand < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rand < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 4000);
   }
