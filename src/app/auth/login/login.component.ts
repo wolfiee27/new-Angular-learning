8a9f4bd9d483093dbe7d284128f13dc526c9bd9d
Gokul <36274337+wolfiee27@users.noreply.github.com>
2024-12-05 14:30:17 +0530

    Onsubmit validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,6 +11,13 @@ import { FormsModule, NgForm } from '@angular/forms';
 export class LoginComponent {
 
   onSubmit(formData: NgForm) {
+
+    if (formData.form.invalid) {
+      // if the form is invalid this returns prematurely,
+      // without executing the form submisson logic
+      return;
+    }
+
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
 
