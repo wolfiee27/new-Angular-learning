dbb2c7909b647980fdaf0293698b0f4912bc6e12
Gokul <36274337+wolfiee27@users.noreply.github.com>
2024-12-05 14:27:01 +0530

    template driven Form control , using directives for form validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule]
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  onSubmit(formData: NgForm) {
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+
+    console.log(enteredEmail, enteredPassword)
+  }
+}
