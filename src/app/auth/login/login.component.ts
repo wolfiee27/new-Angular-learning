2974b31bd6cb431724dd83ae1d63c97751fe70d6
Gokul <vannangokul@gmail.com>
2024-12-10 15:37:50 +0530

    reset to starting Project , for reactive form approach

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,59 +1,9 @@
-import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [FormsModule]
 })
-export class LoginComponent {
-  private form = viewChild.required<NgForm>('form')
-  private emailInput = viewChild.required<HTMLInputElement>('email');
-  private destroyRef = inject(DestroyRef);
-
-
-  constructor() {
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('saved-login-form');
-
-      if (savedForm) {
-        const loadedFormData = JSON.parse(savedForm);
-
-        setTimeout(() => {
-          console.log('loadedFormData - ', loadedFormData.email)
-          this.form().controls['email'].setValue(loadedFormData.email);
-        }, 1);
-      }
-
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) => {
-          window.localStorage.setItem('saved-login-form', JSON.stringify({
-            email: value.email
-          }))
-        }
-      })
-
-      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
-    })
-  }
-
-
-  onSubmit(formData: NgForm) {
-
-    if (formData.form.invalid) {
-      // if the form is invalid this returns prematurely,
-      // without executing the form submisson logic
-      return;
-    }
-
-    const enteredEmail = formData.form.value.email;
-    const enteredPassword = formData.form.value.password;
-
-    console.log(enteredEmail, enteredPassword)
-
-    formData.form.reset();
-  }
-}
+export class LoginComponent { }
