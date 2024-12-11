40e052e56e6a69664404b388760fc056738e7b55
Gokul <vannangokul@gmail.com>
2024-12-11 15:54:23 +0530

    form Array

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -45,6 +45,11 @@ export class SignupComponent {
       ('student', {
         validators: [Validators.required]
       }),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
     agree: new FormControl(false, {
       validators: [Validators.required]
     })
