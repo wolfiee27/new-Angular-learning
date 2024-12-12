91f97c3f0ec79a1fdc1c7911e4bb088a0a184606
Gokul <vannangokul@gmail.com>
2024-12-12 14:16:33 +0530

    initial router configuration

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+import { TaskComponent } from "./tasks/task/task.component";
+
+export const routes: Routes = [{
+  path: 'tasks',
+  component: TaskComponent
+}]
