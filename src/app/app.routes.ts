047660c0616a204af86227ee1e7910a109307b68
Gokul <vannangokul@gmail.com>
2024-12-18 15:50:21 +0530

    redirect based on entered url / from routes.ts file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,6 +17,11 @@ export const routes: Routes = [
     component: UserTasksComponent,
     children: [
       {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'prefix'
+      },
+      {
         path: 'tasks',
         component: TasksComponent
       },
