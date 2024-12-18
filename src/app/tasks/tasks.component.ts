bbea00408d79a5fe1b1702a41f4f67b2e95286fa
Gokul <vannangokul@gmail.com>
2024-12-18 14:57:39 +0530

    fetching tasks based on user Id

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -11,6 +12,8 @@ import { Task } from './task/task.model';
   imports: [TaskComponent],
 })
 export class TasksComponent {
-  userTasks: Task[] = [];
   userId = input.required<string>();
+  private tasksService = inject(TasksService);
+  userTasks = computed(() => this.tasksService.allTasks().filter(task => task.userId === this.userId()))
+
 }
