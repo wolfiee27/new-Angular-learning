f39a9aa06a4e4e520c54a9106358e1d26e328cf8
Gokul <vannangokul@gmail.com>
2024-12-18 15:52:54 +0530

    splitting routes file based on children prop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from "@angular/router";
 
+import { routes as userRoutes } from "./users/users.routes";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 
@@ -15,21 +14,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'prefix'
-      },
-      {
-        path: 'tasks',
-        component: TasksComponent
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent
-      }
-    ]
+    children: userRoutes
   },
   {
     path: '**',
