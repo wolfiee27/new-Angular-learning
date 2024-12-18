ddd0213fe32c23490fe6df0a34bf4586d5899dea
Gokul <vannangokul@gmail.com>
2024-12-18 15:04:50 +0530

    based on relative position  navigate one step back in the url

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { TasksService } from '../tasks.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
@@ -18,6 +19,7 @@ export class NewTaskComponent {
   private tasksService = inject(TasksService);
 
   onSubmit() {
+    console.log('new task added');
     this.tasksService.addTask(
       {
         title: this.enteredTitle(),
