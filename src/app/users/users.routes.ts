3c1f7d8a3e2fb7248f76f1dac28e3c9b50dc2c6b
Gokul <36274337+wolfiee27@users.noreply.github.com>
2025-01-10 17:06:19 +0530

    run the resolver fn when change in params or query

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -12,6 +12,7 @@ export const routes: Routes = [
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
     component: TasksComponent,
+    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
     resolve: {
       userTasks: resolveUserTasks,
     },
